2593ba789ec986f60c3f4e0791c998f7df3b43ee
Charddh <160427482+Charddh@users.noreply.github.com>
2024-10-27 20:39:09 +0000

    Added cluster and bcg catalogue functionality

--- main.py
+++ main.py
@@ -7,37 +7,80 @@ import glob #allows you to read the names of files in folders
 from scipy.optimize import curve_fit
 from astropy.io import fits
 from astropy.table import Table
- 
-with fits.open('GZDR1SFRMASS.fits') as hdu:
-    asn_table = Table(hdu[1].data)
-print(asn_table[1])
 
-with fits.open('GZDR1SFRMASS.fits') as hdu:
-    science_data1 = hdu[1].data
-    science_header1 = hdu[1].header
-#print(science_data1[0])
-#print(science_data1[0][13])
-#print(science_data1[0][14])
+max_z = 0.1 #maximum redshift of galaxies being looked for
+min_n = 30 #minimum companions according to cluster catalogue
+
+with fits.open('catCluster-SPIDERS_RASS_CLUS-v3.0.fits') as cluster:
+    cluster_table = Table(cluster[1].data)
+    cluster_data = cluster[1].data
+#print(cluster_table[1])
+
+with fits.open('SpidersXclusterBCGs-v2.0.fits') as bcg:
+    bcg_table = Table(bcg[1].data)
+    bcg_data = bcg[1].data
+#print(bcg_table)
+
+with fits.open('GZDR1SFRMASS.fits') as galzoo:
+    galzoo_table = Table(galzoo[1].data)
+    galzoo_data = galzoo[1].data
+#print(asn_table[1])
+
+cluster_id = []
+cluster_ra = []
+cluster_dec = []
+cluster_z = []
+cluster_n = []
+
+for i in range(len(cluster_data)):
+    if cluster_data[i][12] < max_z and cluster_data[i][18] > min_n:
+        cluster_id.append(cluster_data[i][0])
+        cluster_ra.append(cluster_data[i][3])
+        cluster_dec.append(cluster_data[i][4])
+        cluster_z.append(cluster_data[i][12])
+        cluster_n.append(cluster_data[i][18])
+
+"""for i in range(len(bcg_data)):
+    print(bcg_data[i][0])"""
+#print(cluster_id)
+
+reduced_clusters_id = []
+reduced_clusters_z = []
+reduced_clusters_ra = []
+reduced_clusters_dec = []
+
+for i in range(len(bcg_data)):
+    for id1 in cluster_id:
+        if id1 == bcg_data[i][0]:
+            reduced_clusters_id.append(bcg_data[i][0])
+            reduced_clusters_z.append(bcg_data[i][1])
+            reduced_clusters_ra.append(bcg_data[i][2])
+            reduced_clusters_dec.append(bcg_data[i][3])
+
+reduced_clusters_locals = []
 
 ID = []
 RA = []
 DEC = []
 z = []
-RAc = 18.73997
-DECc = 0.43081
 
-for i in range(len(science_data1)):
-    ID.append(science_data1[i][0])
-    RA.append(science_data1[i][13])
-    DEC.append(science_data1[i][14])
-    z.append(science_data1[i][18])
-
-local_galaxies = []
+for i in range(len(galzoo_data)):
+    ID.append(galzoo_data[i][0])
+    RA.append(galzoo_data[i][13])
+    DEC.append(galzoo_data[i][14])
+    z.append(galzoo_data[i][18])
 
 for i in range(len(ID)):
-    if np.sqrt(((RAc - RA[i]) ** 2) * (np.cos(DECc) ** 2) + ((DECc - DEC[i]) ** 2)) < 0.31 and 0.0403 < z[i] < 0.0503:
-            local_galaxies.append(ID[i])
+    for j in range(len(reduced_clusters_id)):
+        templist = []
+        if np.sqrt(((reduced_clusters_ra[j] - RA[i]) ** 2) * (np.cos(reduced_clusters_dec[j]) ** 2) + ((reduced_clusters_dec[j] - DEC[i]) ** 2)) < 0.31 and 0.0403 < z[i] < 0.0503:
+            templist.append(ID[i])
+    reduced_clusters_locals.append(templist)
 
-print(len(local_galaxies))
+print(len(reduced_clusters_id))
+print(reduced_clusters_locals)
+print(len(reduced_clusters_locals))
+
+
 
 
