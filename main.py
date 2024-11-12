0390cba6c6d2e33a47d41bae91378e5acd7faf9e
Charddh <160427482+Charddh@users.noreply.github.com>
2024-11-12 10:47:51 +0000

    main_array.py replaces main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import glob #allows you to read the names of files in folders
 from scipy.optimize import curve_fit
 from astropy.io import fits
 from astropy.table import Table
+import pandas as pd
 cosmo = FlatLambdaCDM(H0=70., Om0=0.3)
 
 max_z = 0.1 #maximum redshift of galaxies being looked for
@@ -14,72 +15,56 @@ min_n = 30 #minimum companions according to cluster catalogue
 
 with fits.open('catCluster-SPIDERS_RASS_CLUS-v3.0.fits') as cluster:
     cluster_table = Table(cluster[1].data)
-    cluster_data = cluster[1].data
-#print(cluster_table[1])
+    cluster_data = np.array(list(cluster[1].data))
 
 with fits.open('SpidersXclusterBCGs-v2.0.fits') as bcg:
     bcg_table = Table(bcg[1].data)
-    bcg_data = bcg[1].data
-#print(bcg_table)
+    bcg_data = np.array(list(bcg[1].data))
 
 with fits.open('GZDR1SFRMASS.fits') as galzoo:
     galzoo_table = Table(galzoo[1].data)
-    galzoo_data = galzoo[1].data
-#print(asn_table[1])
+    galzoo_data = np.array(Table(galzoo[1].data))
+# Convert the Table to a pandas DataFrame
+galzoo_df = galzoo_table.to_pandas()
+# Convert to a 2D NumPy array
+galzoo_data = galzoo_df.to_numpy()
 
-cluster_id = []
-cluster_ra = []
-cluster_dec = []
-cluster_z = []
-cluster_n = []
+mask = (cluster_data[:, 12].astype(float) < max_z) & (cluster_data[:, 18].astype(float) > min_n)
+cluster_id = cluster_data[mask, 0]
+cluster_ra = cluster_data[mask, 3]
+cluster_dec = cluster_data[mask, 4]
+cluster_z = cluster_data[mask, 12]
+cluster_n = cluster_data[mask, 18]
 
-for i in range(len(cluster_data)):
-    if cluster_data[i][12] < max_z and cluster_data[i][18] > min_n:
-        cluster_id.append(cluster_data[i][0])
-        cluster_ra.append(cluster_data[i][3])
-        cluster_dec.append(cluster_data[i][4])
-        cluster_z.append(cluster_data[i][12])
-        cluster_n.append(cluster_data[i][18])
+mask = np.isin(bcg_data[:, 0], cluster_id)
+reduced_clusters_id = bcg_data[mask, 0].astype(float)
+reduced_clusters_z = bcg_data[mask, 1].astype(float)
+reduced_clusters_ra = bcg_data[mask, 2].astype(float)
+reduced_clusters_dec = bcg_data[mask, 3].astype(float)
 
-reduced_clusters_id = []
-reduced_clusters_z = []
-reduced_clusters_ra = []
-reduced_clusters_dec = []
-
-for i in range(len(bcg_data)):
-    for id1 in cluster_id:
-        if id1 == bcg_data[i][0]:
-            reduced_clusters_id.append(bcg_data[i][0])
-            reduced_clusters_z.append(bcg_data[i][1])
-            reduced_clusters_ra.append(bcg_data[i][2])
-            reduced_clusters_dec.append(bcg_data[i][3])
+gz_id = galzoo_data[:, 0]
+gz_ra = galzoo_data[:, 13]
+gz_dec = galzoo_data[:, 14]
+gz_z = galzoo_data[:, 18]
 
 reduced_clusters_locals = []
 
-ID = []
-RA = []
-DEC = []
-z = []
+"""print(reduced_clusters_ra)
+print(np.sqrt(((reduced_clusters_ra[0] - gz_ra[0]) ** 2) * (np.cos(reduced_clusters_dec[0]) ** 2) + ((reduced_clusters_dec[0] - gz_dec[0]) ** 2)))
+print(((1000 / 3600) * cosmo.arcsec_per_kpc_proper(float(0.0453)).value))
+print((reduced_clusters_z[0] - 0.01))
+print(gz_z[0])"""
 
-for i in range(len(galzoo_data)):
-    ID.append(galzoo_data[i][0])
-    RA.append(galzoo_data[i][13])
-    DEC.append(galzoo_data[i][14])
-    z.append(galzoo_data[i][18])
+ang_diff = []
+z_diff = []
 
-print(len(reduced_clusters_ra))
-#print(reduced_clusters_dec)
-print(len(RA))
-#print(DEC)
-for i in range(len(ID)):
-    #print(z[i])
-    #print(cosmo.arcsec_per_kpc_proper(z[i]).value)
-    for j in range(len(reduced_clusters_id)):
-        templist = []
-        if np.sqrt(((reduced_clusters_ra[j] - RA[i]) ** 2) * (np.cos(reduced_clusters_dec[j]) ** 2) + ((reduced_clusters_dec[j] - DEC[i]) ** 2)) < ((1000 / 3600) * cosmo.arcsec_per_kpc_proper(z[i]).value) and (z[i] - 0.01) < z[i] < (z[i] + 0.01):
-            templist.append(ID[i])
-        reduced_clusters_locals.append(templist)
+for j in range(len(reduced_clusters_id)):
+    templist = []
+    for i in range(len(gz_id)):
+        if (np.sqrt(((reduced_clusters_ra[j] - gz_ra[i]) ** 2) * (np.cos(reduced_clusters_dec[j]) ** 2) + ((reduced_clusters_dec[j] - gz_dec[i]) ** 2)) < ((1000 / 3600) * cosmo.arcsec_per_kpc_proper(float(reduced_clusters_z[j])).value)) and ((reduced_clusters_z[j] - 0.01) < gz_z[i] < (reduced_clusters_z[j] + 0.01)):
+            templist.append(gz_id[i])
+    reduced_clusters_locals.append(templist)
 
 print(len(reduced_clusters_id))
 print(reduced_clusters_locals)
-print(len(reduced_clusters_locals))
+print(reduced_clusters_locals.shape)
